786997277fe0afc9e70a602b60365b2d65917000
PISETH Sattya <pisethsattya33@gmail.com>
2024-06-03 15:25:25 +0700

    update project

--- src/component/counter.js
+++ src/component/counter.js
@@ -1,61 +1,63 @@
 export const Counter = () => {
     return(`
-<div class="container mx-auto">
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-        <!-- Counter item -->
-        <div class="col-sm-6 col-xl-3">
-            <div class="flex justify-center items-center p-4 bg-yellow-400 bg-opacity-50 rounded-lg transition duration-500 ease-in-out transform hover:scale-105">
-                <span class="text-6xl text-yellow-500"><i class="fas fa-tv"></i></span>
-                <div class="ml-4">
-                    <div class="flex items-center">
-                        <h5 class="purecounter text-2xl font-bold mb-0" data-purecounter-start="0" data-purecounter-end="10" data-purecounter-delay="200" data-purecounter-duration="0">10</h5>
-                        <span class="text-2xl font-bold text-gray-700">K</span>
-                    </div>
-                    <p class="mb-0 text-gray-700">Online Movies</p>
+        <div class="container mx-auto">
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
+                  <!-- Counter item -->
+    <div class="col-span-1 sm:col-span-2 md:col-span-1 lg:col-span-1 xl:col-span-1">
+        <div class="flex justify-center items-center p-4 bg-yellow-100 dark:bg-gray-900 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+            <span class="text-6xl text-yellow-600 dark:text-yellow-300 px-4"><i class="fas fa-tv"></i></span>
+            <div class="ml-4">
+                <div class="flex items-center justify-center">
+                    <h5 class="purecounter text-2xl font-bold mb-0 dark:text-white" data-purecounter-start="0" data-purecounter-end="10" data-purecounter-delay="200" data-purecounter-duration="0">10</h5>
+                    <span class="text-2xl font-bold text-gray-900 dark:text-white">K</span>
                 </div>
-            </div>
-        </div>
-        <!-- Counter item -->
-        <div class="col-sm-6 col-xl-3">
-            <div class="flex justify-center items-center p-4 bg-blue-500 bg-opacity-10 rounded-lg transition duration-500 ease-in-out transform hover:scale-105">
-                <span class="text-6xl text-blue-500"><i class="fas fa-user-tie"></i></span>
-                <div class="ml-4">
-                    <div class="flex items-center">
-                        <h5 class="purecounter text-2xl font-bold mb-0" data-purecounter-start="0" data-purecounter-end="200" data-purecounter-delay="200" data-purecounter-duration="0">200</h5>
-                        <span class="text-2xl font-bold text-gray-700">+</span>
-                    </div>
-                    <p class="mb-0 text-gray-700">Expert Tutors</p>
-                </div>
-            </div>
-        </div>
-        <!-- Counter item -->
-        <div class="col-sm-6 col-xl-3">
-            <div class="flex justify-center items-center p-4 bg-purple-500 bg-opacity-10 rounded-lg transition duration-500 ease-in-out transform hover:scale-105">
-                <span class="text-6xl text-purple-500"><i class="fas fa-user-graduate"></i></span>
-                <div class="ml-4">
-                    <div class="flex items-center">
-                        <h5 class="purecounter text-2xl font-bold mb-0" data-purecounter-start="0" data-purecounter-end="60" data-purecounter-delay="200" data-purecounter-duration="0">60</h5>
-                        <span class="text-2xl font-bold text-gray-700">K+</span>
-                    </div>
-                    <p class="mb-0 text-gray-700">Online Students</p>
-                </div>
-            </div>
-        </div>
-        <!-- Counter item -->
-        <div class="col-sm-6 col-xl-3">
-            <div class="flex justify-center items-center p-4 bg-blue-500 bg-opacity-10 rounded-lg transition duration-500 ease-in-out transform hover:scale-105">
-                <span class="text-6xl text-blue-500"><i class="bi bi-patch-check-fill"></i></span>
-                <div class="ml-4">
-                    <div class="flex items-center">
-                        <h5 class="purecounter text-2xl font-bold mb-0" data-purecounter-start="0" data-purecounter-end="6" data-purecounter-delay="300" data-purecounter-duration="0">6</h5>
-                        <span class="text-2xl font-bold text-gray-700">K+</span>
-                    </div>
-                    <p class="mb-0 text-gray-700">Certified Movies</p>
-                </div>
+                <p class="text-gray-900 dark:text-gray-300 mt-2">Online Movies</p>
             </div>
         </div>
     </div>
-</div>
+    
+        <!-- Counter item -->
+        <div class="col-span-1 sm:col-span-2 md:col-span-1 lg:col-span-1 xl:col-span-1">
+            <div class="flex justify-center items-center p-4 bg-blue-100 dark:bg-gray-900 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+                <span class="text-6xl text-blue-600 dark:text-blue-300 px-4"><i class="fas fa-user-tie"></i></span>
+                <div class="ml-4">
+                    <div class="flex items-center">
+                        <h5 class="purecounter text-2xl font-bold mb-0 dark:text-white" data-purecounter-start="0" data-purecounter-end="200" data-purecounter-delay="200" data-purecounter-duration="0">200</h5>
+                        <span class="text-2xl font-bold text-gray-900 dark:text-white">+</span>
+                    </div>
+                    <p class="text-gray-900 dark:text-gray-300 mt-2">Expert Tutors</p>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Counter item -->
+        <div class="col-span-1 sm:col-span-2 md:col-span-1 lg:col-span-1 xl:col-span-1">
+            <div class="flex justify-center items-center p-4 bg-purple-100 dark:bg-gray-900 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+                <span class="text-6xl text-purple-600 dark:text-purple-300 px-4"><i class="fas fa-user-graduate"></i></span>
+                <div class="ml-4">
+                    <div class="flex items-center">
+                        <h5 class="purecounter text-2xl font-bold mb-0 dark:text-white" data-purecounter-start="0" data-purecounter-end="60" data-purecounter-delay="200" data-purecounter-duration="0">60</h5>
+                        <span class="text-2xl font-bold text-gray-900 dark:text-white">K+</span>
+                    </div>
+                    <p class="text-gray-900 dark:text-gray-300 mt-2">Online Students</p>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Counter item -->
+        <div class="col-span-1 sm:col-span-2 md:col-span-1 lg:col-span-1 xl:col-span-1">
+            <div class="flex justify-center items-center p-4 bg-blue-100 dark:bg-gray-900 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+                <span class="text-6xl text-blue-600 dark:text-blue-300 px-4"><i class="bi bi-patch-check-fill"></i></span>
+                <div class="ml-4">
+                    <div class="flex items-center">
+                        <h5 class="purecounter text-2xl font-bold mb-0 dark:text-white" data-purecounter-start="0" data-purecounter-end="6" data-purecounter-delay="300" data-purecounter-duration="0">6</h5>
+                        <span class="text-2xl font-bold text-gray-900 dark:text-white">K+</span>
+                    </div>
+                    <p class="text-gray-900 dark:text-gray-300 mt-2">Certified Movies</p>
+                </div>
+            </div>
+        </div>
 
+        </div>
     `);
 }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,7 +2,7 @@ import {Navbar} from "../component/navbar.js";
 import {Banner} from "../component/banner.js";
 import {Content} from "../component/content.js";
 import {movies} from "../data/api-movie.js";
-import { Card } from "../component/card.js";
+import {Card} from "../component/card.js";
 import {Counter} from "../component/counter.js";
 import {Footer} from "../component/footer.js";
 
@@ -18,6 +18,48 @@ render_banner.innerHTML = Banner();
 render_content.innerHTML = Content();
 render_counter.innerHTML = Counter();
 render_footer.innerHTML = Footer();
-render_card.innerHTML = movies.map((movie) => {
-    return Card(movie);
-})
+movies.map((movie) => {
+   return render_card.innerHTML += Card(movie);
+});
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function () {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+
+});
+
