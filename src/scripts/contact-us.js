91f15c7f6bef793b4b4ca390187d192cd8e74230
PISETH Sattya <pisethsattya33@gmail.com>
2024-06-04 11:48:39 +0700

    testing

--- src/scripts/contact-us.js
+++ src/scripts/contact-us.js
@@ -4,7 +4,7 @@ import {Contact} from "../component/contact.js";
 let nav = `
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+  <a href="./../index.html" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="https://api.istad.co/media/image/c8c41751-3bc0-4f07-9658-7d95efbae692.png" class="h-8" alt="Flowbite Logo">
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SPRING CINEMA</span>
   </a>
